d5cde65409e682266946c5ec1a9457da29d7b397
Henrique Melanda <henrique.melanda@fcamara.com.br>
2018-09-03 09:12:59 -0300

    First commit with base project

---
+++ src/app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+import React, { PureComponent } from 'react'
+
+class App extends PureComponent {
+  render () {
+    return (
+      <div>
+        <h1>My Help Packet</h1>
+      </div>
+    )
+  }
+}
+
+export default App
