91ff898f437c7b44f48594be78572c4ab880a1a7
Henrique Melanda <hjdesigner23@gmail.com>
2018-09-23 23:44:26 -0300

    Added lib chat js and created view mounth graphic

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,52 +1,156 @@
 'use strict'
 import React from 'react'
+import { Pie, Doughnut } from 'react-chartjs-2'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faWallet, faArchive, faBalanceScale, faDollarSign, faMoneyCheckAlt, faUser } from '@fortawesome/free-solid-svg-icons'
 
-const Home = () => (
-  <HomeContainer>
-    <Container>
-      <Nav>
-        <NavUl>
-          <NavList>
-            <NavLink to='/'><FontAwesomeIcon icon={faWallet} />Visão Geral</NavLink>
-          </NavList>
-          <NavList>
-            <NavLink to='/'><FontAwesomeIcon icon={faArchive} />Categoria</NavLink>
-          </NavList>
-          <NavList>
-            <NavLink to='/'><FontAwesomeIcon icon={faBalanceScale} />Despesa</NavLink>
-          </NavList>
-          <NavList>
-            <NavLink to='/'><FontAwesomeIcon icon={faDollarSign} />Receita</NavLink>
-          </NavList>
-          <NavList>
-            <NavLink to='/'><FontAwesomeIcon icon={faMoneyCheckAlt} />Investimento</NavLink>
-          </NavList>
-          <NavList>
-            <NavLink to='/'><FontAwesomeIcon icon={faUser} />Minha Conta</NavLink>
-          </NavList>
-        </NavUl>
-      </Nav>
-    </Container>
-  </HomeContainer>
-)
+const Home = () => {
+  const items = {
+    labels: [
+      'Red',
+      'Green',
+      'Yellow'
+    ],
+    datasets: [{
+      data: [300, 50, 100],
+      backgroundColor: [
+        '#FF6384',
+        '#36A2EB',
+        '#FFCE56'
+      ],
+      hoverBackgroundColor: [
+        '#FF6384',
+        '#36A2EB',
+        '#FFCE56'
+      ]
+    }]
+  }
+  return (
+    <HomeContainer>
+      <Container>
+        <Nav>
+          <NavUl>
+            <NavList>
+              <NavLink to='/'><FontAwesomeIcon icon={faWallet} />Visão Geral</NavLink>
+            </NavList>
+            <NavList>
+              <NavLink to='/'><FontAwesomeIcon icon={faArchive} />Categoria</NavLink>
+            </NavList>
+            <NavList>
+              <NavLink to='/'><FontAwesomeIcon icon={faBalanceScale} />Despesa</NavLink>
+            </NavList>
+            <NavList>
+              <NavLink to='/'><FontAwesomeIcon icon={faDollarSign} />Receita</NavLink>
+            </NavList>
+            <NavList>
+              <NavLink to='/'><FontAwesomeIcon icon={faMoneyCheckAlt} />Investimento</NavLink>
+            </NavList>
+            <NavList>
+              <NavLink to='/'><FontAwesomeIcon icon={faUser} />Minha Conta</NavLink>
+            </NavList>
+          </NavUl>
+        </Nav>
+        <MonthlyValue>
+          <Months>Janeiro</Months>
+          <Graphic>
+            <GraphicTitle>Despesas</GraphicTitle>
+            <Doughnut
+              data={items}
+              options={{
+                legend: {
+                  position: 'bottom',
+                  lineWidth: 10,
+                  labels: {
+                    boxWidth: 20
+                  }
+                }
+              }} />
+          </Graphic>
+          <Graphic>
+            <GraphicTitle>Receita</GraphicTitle>
+            <Pie data={items} />
+          </Graphic>
+        </MonthlyValue>
+      </Container>
+    </HomeContainer>
+  )
+}
 
+const Graphic = styled.div`
+  margin-bottom: 20px;
+  position: relative;
+  @media screen and (min-width: 1024px) {
+    width: 49%;
+  }
+`
+const GraphicTitle = styled.h2`
+  text-align: center;
+  margin: 20px 0 10px;
+  font-size: 1em;
+`
 const HomeContainer = styled.main``
 const Container = styled.div`
+  max-width: 1360px;
   display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
   padding-right: 15px;
   padding-left: 15px;
   margin-right: auto;
   margin-left: auto;
 `
 
+const MonthlyValue = styled.section`
+  width: 100%;
+  margin-top: 20px;
+  margin-bottom: 60px;
+  background-color: #FFF;
+  color: #000;
+  border: 1px solid #e6e6e6;
+  @media screen and (min-width: 768px) {
+    width: 49%;
+    margin-top: 0px;
+  }
+  @media screen and (min-width: 1024px) {
+    width: 63%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+`
+const Months = styled.div`
+  width: 100%;
+  margin: 10px 0;
+  text-align: center;
+  font-weight: 600;
+  font-size: 1.2em;
+  color: #57d3ff;
+  &:before,
+  &:after {
+    content: "";
+    width: 7px;
+    height: 7px;
+    display: inline-block;
+    border: 3px solid #CCC;
+    border-right: 0;
+    border-bottom: 0;
+  }
+  &:before {
+    margin-right: 10px;
+    transform: rotate(-45deg)
+  }
+  &:after {
+    margin-left: 10px;
+    transform: rotate(145deg)
+  }
+`
+
 const Nav = styled.section`
   width: 100%;
   @media screen and (min-width: 768px) {
-    width: 50%;
+    width: 49%;
   }
   @media screen and (min-width: 1024px) {
     width: 36%;
